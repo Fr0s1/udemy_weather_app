2e6449b0d493b8b6650f2c55992088a4220bdeff
frost <dthieu223@gmail.com>
2020-10-07 22:20:02 +0700

    changes for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     
     const location = search.value
 
-    fetch('http://127.0.0.1:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 locationName.textContent = data.error;
